72f24bf6ef2448adb1f97dda5852d57d5980df41
Tatiana Kotenko <tattikot15@gmail.com>
2025-08-11 14:07:51 +0300

    first

---
+++ 06K_lesson/test_01_form.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+path = r'C:/Users/tatti/Desktop/Driver/msedgedriver.exe'
+service = Service(executable_path=path)
+driver = webdriver.Edge(service=service)
+
+from time import sleep
+driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
+
+driver.implicitly_wait(10)
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="first-name"]')
+search_box.send_keys("Иван")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="last-name"]')
+search_box.send_keys("Петров")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="address"]')
+search_box.send_keys("Ленина, 55-3")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="e-mail"]')
+search_box.send_keys("test@skypro.com")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="phone"]')
+search_box.send_keys("+7985899998787")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="phone"]')
+search_box.send_keys("+7985899998787")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="zip-code"]')
+search_box.send_keys("")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="city"]')
+search_box.send_keys("Москва")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="country"]')
+search_box.send_keys("россия")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="job-position"]')
+search_box.send_keys("QA")
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[name="company"]')
+search_box.send_keys("SkyPro")
+
+
+submit = driver.find_element(By.CSS_SELECTOR, 'button.btn.btn-outline-primary.mt-3')
+submit.send_keys(Keys.END)
+
+waiter = WebDriverWait(driver, 30).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.btn.btn-outline-primary.mt-3')))
+submit.click()
+
+WebDriverWait(driver, 30).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "alert.py-2.alert-danger")))
+
+zip_code = driver.find_element(By.ID, "zip-code").get_attribute("class")
+assert zip_code == "alert.py-2.alert-danger"
+
+poles = [search_box]
+for pole in poles:
+    pole_class =  driver.find_element(By.CSS_SELECTOR, pole).get_attribute("class")
+
+    assert pole_class == "alert.py-2.alert-success"
+
+driver.quit()
---
+++ 06K_lesson/test_03_shop.py
@@ -0,0 +1,72 @@
+from selenium import webdriver
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class StringUtils:
+    def test_shop():
+        from selenium.webdriver.firefox.service import Service
+        path = r'C:/Users/tatti/Downloads/geckodriver-v0.36.0-win64/geckodriver.exe'
+        service = Service(executable_path=path)
+        options = webdriver.FirefoxOptions()
+        driver = webdriver.Firefox(service=service, options=options)
+
+        driver.get("https://www.saucedemo.com/")
+
+        UserName = driver.find_element(By.CSS_SELECTOR, '[id="user-name"]')
+        UserName.send_keys("standard_user")
+
+        Password = driver.find_element(By.CSS_SELECTOR, '[id="password"]')
+        Password.send_keys("secret_sauce")
+
+        equals_button = WebDriverWait(driver, 20).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='login-button']")))
+        equals_button = driver.find_element(By.CSS_SELECTOR, "[id='login-button']")
+        equals_button.click()
+
+        backpack = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='add-to-cart-sauce-labs-backpack']")))
+        backpack = driver.find_element(By.CSS_SELECTOR, "[id='add-to-cart-sauce-labs-backpack']")
+        backpack.click()
+
+        shirt = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='add-to-cart-sauce-labs-bolt-t-shirt']")))
+        shirt = driver.find_element(By.CSS_SELECTOR, "[id='add-to-cart-sauce-labs-bolt-t-shirt']")
+        shirt.click()
+
+        onesie = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='add-to-cart-sauce-labs-onesie']")))
+        onesie= driver.find_element(By.CSS_SELECTOR, "[id='add-to-cart-sauce-labs-onesie']")
+        onesie.click()
+
+        basket = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "a.shopping_cart_link")))
+        basket = driver.find_element(By.CSS_SELECTOR, "a.shopping_cart_link")
+        basket.click()
+
+        checkout = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='checkout']")))
+        checkout = driver.find_element(By.CSS_SELECTOR, "[id='checkout']")
+        checkout.click()
+
+        first_name = driver.find_element(By.CSS_SELECTOR, '[id="first-name"]')
+        first_name.send_keys("Tatiana")
+
+        last_name = driver.find_element(By.CSS_SELECTOR, '[id="last-name"]')
+        last_name.send_keys("Kotenko")
+
+        postal_codee = driver.find_element(By.CSS_SELECTOR, '[id="postal-code"]')
+        postal_codee.send_keys("283036")
+
+        contin = WebDriverWait(driver, 30).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[id='continue']")))
+        contin = driver.find_element(By.CSS_SELECTOR, "[id='continue']")
+        contin.click()
+
+        text_prise = driver.find_element(By.CSS_SELECTOR, "div.summary_total_label").text
+        text_prise_value = float(text_prise.split("$")[1])
+
+        assert text_prise_value == 58.29
+        driver.quit()
+
